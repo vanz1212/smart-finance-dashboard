e351a3ba5f95feb5a9ae2f609af02f013512c5f4
Ulinnuha Alkindi Rosyadi <alkindi.rosyadi@gmail.com>
2026-05-25 19:43:42 +0700

    FIx error

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
@@ -14,7 +11,12 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
